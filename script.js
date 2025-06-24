adf9dee7c74ccb3fe76024cc7566861de825ddd7
Cauan Bernardino Diogo <devcauanbernardino@gmail.com>
2025-06-24 16:22:57 -0300

    testando

---
+++ script.js
@@ -0,0 +1,7 @@
+var nome = window.prompt('Qual o seu nome? ')
+window.alert(`Olá, ${nome}! Seja bem-vindo!`)
+
+document.writeln(`Seu nome tem ${nome.length} letras. <br>`)
+document.writeln(`Seu nome em maiúsculas é ${nome.toUpperCase()}. <br>`)
+document.writeln(`Seu nome em minúscula é ${nome.toLowerCase()}`)
+
